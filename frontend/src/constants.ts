042aa3c5f30068898396d21c68597c668013b12c
azamatmurzaliev <Lirog.kg@gmail.com>
2025-04-02 12:31:51 +0600

    test4

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
-export const apiUrl = process.env.REACT_APP_API_URL;
+export const apiUrl = process.env.REACT_APP_API_URL + '/api/';
 export const GOOGLE_CLIENT_ID = '1033022370181-dphif33ggrmhgs75vfqp559pec80vthv.apps.googleusercontent.com';
 // export const apiUrl = 'http://api.adilet.club';
 // export const apiUrl = 'http://localhost:8000';
